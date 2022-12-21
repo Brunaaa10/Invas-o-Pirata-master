65a50b1a5f5ba012a4f91685b2f728fcac520f1a
brunaaa10 <brunaleticiadiniz1@gmail.com>
2022-12-20 21:10:56 -0300

    Fase 1

---
+++ js/tower.js
@@ -0,0 +1,23 @@
+class Tower{
+ constructor(x,y,w,h){
+   var options = {
+    isStatic:true
+   }
+   this.w = w
+   this.h = h
+
+   this.image = loadImage("./assets/tower.png")
+
+   this.body = Bodies.rectangle(x,y,w,h, options)
+   World.add(world, this.body)
+ }
+ 
+  display(){
+    var pos = this.body.position;
+
+    push()
+    imageMode(CENTER);
+    image(this.image, pos.x, pos.y, this.w, this.h)
+    pop()
+  }
+}
